beb725618b5b7a5d6402a50a92e8092086eb6bc8
Lalit Max <lalitmay@gmail.com>
2024-06-27 21:51:38 +0530

    prevent appid for firebase and add .env file

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const APPID=process.env.APPID;
 const firebaseConfig = {
   apiKey: "AIzaSyCFQNToBeLRbiX6qwCsck0hCXvZe_Q9JEM",
   authDomain: "chat-app-user-img.firebaseapp.com",
   projectId: "chat-app-user-img",
   storageBucket: "chat-app-user-img.appspot.com",
   messagingSenderId: "904854464974",
-  appId: "1:904854464974:web:2046535b494ebb9378e39f"
+  appId: APPID,
 };
 
 // Initialize Firebase
